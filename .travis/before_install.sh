be5b67172cde48a0b1ecc67c6ca1fa4331c443ae
darkopancev <darkopancev.dev@gmail.com>
2019-10-09 18:06:19 +0800

    add travis ci and codecov

---
+++ .travis/before_install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+die() {
+    echo " *** ERROR: " $*
+    exit 1
+}
+
+set -x
+
+cd /tmp || die
+
+[ $TRAVIS_OS_NAME != linux ] || {
+    sudo apt-get update || die
+    sudo apt-get install lcov || die
+
+    [ $BUILD_TARGET != pretty-check ] || {
+        clang-format --version || die
+    }
+
+    [ $BUILD_TARGET != scan-build ] || {
+        sudo apt-get install clang || die
+    }
+
+    [ $BUILD_TARGET != posix -o $CC != clang ] || {
+        sudo apt-get install clang || die
+    }
+}
+
+[ $TRAVIS_OS_NAME != osx ] || {
+    sudo easy_install pexpect || die
+}
---
+++ .travis/script.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+die() {
+    echo " *** ERROR: " $*
+    exit 1
+}
+
+set -x
+
+[ $BUILD_TARGET != pretty-check ] || {
+    ./bootstrap || die
+    ./configure || die
+    make pretty-check || die
+}
+
+[ $BUILD_TARGET != scan-build ] || {
+    ./bootstrap || die
+
+    scan-build ./configure              \
+        --enable-cli-app=all            \
+        --with-examples=posix || die
+    scan-build --status-bugs -analyze-headers -v make || die
+}
+
+[ $BUILD_TARGET != posix ] || {
+    sh -c '$CC --version' || die
+    sh -c '$CXX --version' || die
+
+    git checkout -- . || die
+    git clean -xfd || die
+    ./bootstrap || die
+    CPPFLAGS=-DCONFIG_LOG_LEVEL=LOG_LEVEL_NONE make -f examples/Makefile-posix || die
+
+    git checkout -- . || die
+    git clean -xfd || die
+    ./bootstrap || die
+    CPPFLAGS=-DCONFIG_LOG_LEVEL=LOG_LEVEL_DEBG make -f examples/Makefile-posix || die
+}
